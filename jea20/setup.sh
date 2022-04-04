ccc7feb71bc344ed6e1ece4429bac4b58e1dc9d9
Lars Gottesbüren <lars.gottesbueren@kit.edu>
2022-04-04 16:38:20 +0200

    update link to kahypar repo and hard-code compiler

--- jea20/setup.sh
+++ jea20/setup.sh
@@ -1,10 +1,10 @@
-git clone --recursive git@github.com:kahypar/kahypar.git
+git clone --recursive https://github.com/kahypar/kahypar.git
 cd kahypar 
 git checkout 876b7768a92614747e6427e596b89506349c7561
 cd external_tools/WHFC
 git checkout 1a787e9f7dfd29dca3037beb9959fcec8756bce0
 cd ../../ && mkdir release && cd release
-cmake .. -DCMAKE_BUILD_TYPE=Release
+CXX=/software/gcc/9.3.0/bin/g++ cmake .. -DCMAKE_BUILD_TYPE=Release
 make KaHyPar
 cd ../../
 curl -o benchmark_set.tar https://algo2.iti.kit.edu/schlag/sea2017/benchmark_set.tar
